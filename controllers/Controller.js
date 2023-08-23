f715800d6974bf110c6a667ea16b773fe18eec70
TERPHK <phakkharaphon23@gmail.com>
2023-08-23 13:39:24 +0700

    que1

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -2,7 +2,7 @@ const Model = require('../models/Model');
 
 const que1Controller = (req, res) => {
     const { number } = req.body;
-    const que1 = Model.multiply(number) +","+ Model.plus(number)+","+ Model.plusplus(number)+","+ Model. deletedelete(number)+","+ Model. per(number) ;
+    const que1 =  Model. deletedelete(number);
    
     res.json({ que1 });
 };
--- models/Model.js
+++ models/Model.js
@@ -1,18 +1,6 @@
 class model {
-  multiply(number) {
-      return number*number
-    }
-    plus(number) {
-      return number+number
-    }
     deletedelete(number) {
       return number--
-    }
-    plusplus(number) {
-      return number++
-    }
-    per(number) {
-      return number%2
     }
    
 }
